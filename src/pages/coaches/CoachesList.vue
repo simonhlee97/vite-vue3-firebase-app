99fff3f6c32e127bec03341b03529284f07160e0
Simon Lee <simonhlee97@hotmail.com>
2022-05-16 17:13:51 +0900

    vue lesson Filtering Coaches done

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -46,8 +46,19 @@ export default {
   },
   computed: {
     filteredCoaches() {
-      return this.$store.getters['coaches/coaches']
-      // ['namespace / getter']
+      const coaches = this.$store.getters['coaches/coaches']
+      return coaches.filter(coach => {
+        if (this.activeFilters.frontend && coach.areas.includes('frontend')) {
+          return true;
+        }
+        if (this.activeFilters.backend && coach.areas.includes('backend')) {
+          return true;
+        }
+        if (this.activeFilters.career && coach.areas.includes('career')) {
+          return true;
+        }
+        return false;
+      })
     },
     hasCoaches() {
       return this.$store.getters['coaches/hasCoaches']

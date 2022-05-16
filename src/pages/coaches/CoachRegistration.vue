763d422bfd3129afea74eae0822903e652a14e92
Simon Lee <simonhlee97@hotmail.com>
2022-05-16 17:34:30 +0900

    vue lesson Register a Coach Form done

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,11 +1,22 @@
 <template>
 
-COACH REGISTRATION PAGE
+  <section>
+    <base-card>
+      <h2>Register as a Coach</h2>
+      <CoachForm></CoachForm> 
+    </base-card>
+  </section>
 
 </template>
 
 <script>
+import CoachForm from "../../components/coaches/CoachForm.vue";
 
+export default {
+  components: {
+    CoachForm
+  }
+}
 </script>
 
 <style scoped>

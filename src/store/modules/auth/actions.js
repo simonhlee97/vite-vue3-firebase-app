2ab651e0eaf8ffbf151ca0aaaf8958fcc36ffc25
Simon Lee <simonhlee97@hotmail.com>
2022-05-18 15:17:36 +0900

    267 Adding Login Action and Flow

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,6 +1,29 @@
 
 export default {
-  login() { },
+  async login(context, payload) {
+    const response = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${import.meta.env.VITE_FIREBASE_KEY}`, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true
+      })
+    });
+     const responseData = await response.json();
+
+    if (!response.ok) {
+      console.log(responseData);
+      const error = new Error(responseData.message || 'Could not Login. Failed to Authenticate');
+      throw error;
+    }
+    console.log(responseData);
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn
+    })
+  },
+
   async signup(context, payload) { 
 
     const response = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${import.meta.env.VITE_FIREBASE_KEY}`, {
